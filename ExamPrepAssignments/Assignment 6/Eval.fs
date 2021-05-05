103e730cd12aa9ca6a735fbf3cc4319592ca638c
Mikkel Luja Rasmussen <mlr@redbarnet.dk>
2021-05-05 19:36:59 +0200

    Ass6 - Part 3 - Completed

--- ExamPrepAssignments/Assignment 6/Eval.fs
+++ ExamPrepAssignments/Assignment 6/Eval.fs
@@ -136,11 +136,143 @@ module Eval =
         
     let prog = new StateBuilder()
 
-    let arithEval2 a = failwith "Not implemented"
-    let charEval2 c = failwith "Not implemented"
-    let rec boolEval2 b = failwith "Not implemented"
+    let rec arithEval2 a = 
+        match a with
+        | N n -> prog.Return n
+        | V x -> 
+            prog {
+                return! (lookup x)
+            } 
+        | PV x -> 
+            prog { 
+                let! x = (arithEval2 x)
+                return! pointValue x 
+            }
+        | WL -> wordLength
+        | Add (a1, a2) ->
+            prog {
+                let! i1 = arithEval2 a1
+                let! i2 = arithEval2 a2
+                return i1 + i2
+            }
+        | Sub (a1, a2) -> 
+            prog {
+                let! i1 = arithEval2 a1
+                let! i2 = arithEval2 a2
+                return i1 - i2
+            }
+        | Mul (a1, a2) -> 
+            prog {
+                let! i1 = arithEval2 a1
+                let! i2 = arithEval2 a2
+                return i1 * i2
+            }
+        | Div (a1, a2) -> 
+            prog {
+                return! div (arithEval2 a1) (arithEval2 a2)
+            }
+        | Mod (a1, a2) -> 
+            prog {
+                return! divisor (%) (arithEval2 a1) (arithEval2 a2)
+            }
+        | CharToInt c  ->
+            prog { 
+                let! x = (charEval c)
+                return (int x) 
+            }
+    let rec charEval2 c = 
+        match c with
+        | C x -> prog.Return x
+        | CV x ->
+            prog { 
+                let! x = (arithEval2 x)
+                return! characterValue x 
+            }
+        | ToLower x ->
+            prog { 
+                let! c = (charEval2 x)
+                return Char.ToLower(c) 
+            }
+        | ToUpper x ->
+            prog { 
+                let! c = (charEval2 x)
+                return Char.ToUpper(c) 
+            }
+        | IntToChar x ->
+            prog { 
+                let! a = (arithEval x)
+                return (char a) 
+            }
+    let rec boolEval2 b = 
+        match b with
+        | TT -> prog.Return true
+        | FF ->  prog.Return false
+        | AEq (x,y) ->
+            prog {
+                return! (binop (=) (arithEval2 x) (arithEval2 y))
+            }
+        | ALt (x,y) ->
+            prog {
+                return! (binop (<) (arithEval2 x) (arithEval2 y))
+            }
+        | Not bx -> 
+            prog {
+                return! (unop (not) (boolEval2 bx))
+            }
+        | Conj (bx, by) ->
+            prog {
+                return! (binop (&&) (boolEval2 bx) (boolEval2 by))
+            }
+        | IsLetter cx ->
+            prog { 
+                let! a = (charEval2 cx)
+                return Char.IsLetter(a) 
+            }
+        | IsDigit cx ->
+            prog { 
+                let! a = (charEval2 cx)
+                return Char.IsDigit(a) 
+            }
 
-    let stmntEval2 stm = failwith "Not implemented"
+    let rec stmntEval2 stm = 
+        match stm with
+        | Declare s -> 
+            prog {
+                do! declare s
+            }
+        | Ass (s, a) ->
+            prog {
+                let! x = arithEval2 a
+                do! update s x
+            }
+        | Skip -> prog.Return ()
+        | Seq (stm1, stm2) ->
+            prog {
+                do! stmntEval2 stm1
+                do! stmntEval2 stm2
+            }
+        | ITE (bExp1, stm1, stm2) -> 
+            prog { //boolEval bExp1 >>= fun t -> push >>>= (if t then stmntEval stm1 else stmntEval stm2) >>>= pop
+                let! b = boolEval2 bExp1
+                do! push
+                do! (if b then stmntEval stm1 else stmntEval stm2)
+                do! pop
+            }
+        | While (bExp1, stm1) -> //boolEval bExp1 >>= fun t -> push >>>= (if t then stmntEval stm1 >>>= stmntEval stmnt else ret ()) >>>= pop
+             prog {
+                let! b = boolEval2 bExp1
+                do! push
+                do! (
+                    if b then 
+                        prog {
+                            do! stmntEval stm1 
+                            do! stmntEval stm 
+                        }
+                    else prog.Return ()
+                )
+                do! pop
+             }
+
 
 (* Part 4 *) 
 
